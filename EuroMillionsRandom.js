0bf0b4fe9dcc34aae475dfdc1fcc75eb75b6181a
magdyz <magdy_176@hotmail.com>
2022-12-28 11:43:02 +0000

    New Cleaner Code

--- EuroMillionsRandom.js
+++ EuroMillionsRandom.js
@@ -1,49 +1,70 @@
-// create random number function
+// generate random integer number
 
-function createRandomNum( n ) {
-    let random = Math.random();
-    random = Math.floor( random * n ) + 1;
-    return random
-}
+function getRandomNum ( number ){
+  
+  let randomNum = Math.floor(Math.random()*number+1)
+  
+  return randomNum
+  
+  }
 
-function generateNums (firstNumbersCount,secondNumbersCount){
-    let newNum = [];
-    let luckyNum = [];
+
+function generateRandomSet ( firstNumberOfTimes , firstNumbers , secondNumberOfTimes, secondNumber ){
+   
+  let firstNumbersSet = []
+  let secondNumberSet = []
+  
+  
+  for ( let num = 0 ; num < firstNumberOfTimes ; num ++ ) {
     
-    if (typeof(secondNumbersCount)=='undefined'){
-      secondNumbersCount = 0;
+    let number = getRandomNum ( firstNumbers )
+    
+    while ( firstNumbersSet.includes( number )) {
+      
+      number = getRandomNum ( firstNumbers )
+      
     }
-
-    for (let i = 0 ; i < 5 ; i ++){
-        newNum.push( createRandomNum (firstNumbersCount) );
+    
+    firstNumbersSet.push( number )
+    
     }
-
-    for (let i = 0 ; i < 2 ; i ++){
-        firstPick = createRandomNum (secondNumbersCount);
-        if (luckyNum.includes( firstPick )){
-            firstPick = createRandomNum (secondNumbersCount);
-            luckyNum.push( firstPick );
-        } else {
-            luckyNum.push( firstPick );
-        }
+  
+  for ( let num2 = 0 ; num2 < secondNumberOfTimes ; num2 ++ ) { 
+  
+    let number2 = getRandomNum ( secondNumber )
+    
+    while ( secondNumberSet.includes( number2 )) {
+      
+      number2 = getRandomNum ( secondNumber )
+      
     }
-
-  // combine two lists
-    return newNum.concat( luckyNum ) ;
+    
+    secondNumberSet.push ( number2 )
+    
+    }
+  
+  
+    
+  return ( firstNumbersSet.join(" ") + " " + secondNumberSet.join(" ") )
+  
 }
 
-// use function customised for each game
-// for euro millions slice five numbers and last two for lucky nums
+function euro (){ 
 
-const euroMillionsMain = generateNums ( 50 , 12 ).slice ( 0 , 5 ).toLocaleString();
-const euroMillionsLucky = generateNums ( 50 , 12 ).slice ( 5 , 8 ).toLocaleString();
+  return generateRandomSet ( 5 , 50, 2 , 12 )
+  
+}
 
-// for lotto there arent any lucky nums
+function lotto (){
+  
+  return generateRandomSet ( 6 , 59, 0 , 0 )
+  
+}
 
-const lottoNumbers = generateNums ( 59 ).slice ( 0 , 6 ).toLocaleString();
+function setForLife (){
+  
+  return generateRandomSet ( 5 , 47, 1 , 10 )
+  
+}
 
-
-let lotto = "Lotto Numbers: "+lottoNumbers;
-let euroMillions = "EuroMillions number: " + euroMillionsMain + ' ' + "\nLucky number: " + euroMillionsLucky
-
-console.log(lotto + "\n" + euroMillions)
+console.log(setForLife())
